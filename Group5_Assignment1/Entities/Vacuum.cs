7aa5f5e2e86da43e12f203cb5fc6ac78fa6eaa1e
SethG1262 <151973945+SethG1262@users.noreply.github.com>
2024-02-12 21:36:48 -0700

    Initial commit

---
+++ Group5_Assignment1/Entities/Vacuum.cs
@@ -0,0 +1,45 @@
+﻿using Group5_Assignment1.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group5_Assignment1.Entities
+{
+    internal class Vacuum : Appliance
+    {
+        // Fields
+        private readonly string _grade;
+        private readonly short _batteryVoltage;
+
+        // Properties
+        public string Grade { get { return _grade; } }
+        public short BatteryVoltage { get { return _batteryVoltage; } }
+
+        // Constructor
+        public Vacuum(long itemNumber, string brand, int quantity, decimal wattage, string color, decimal price, string grade, short batteryVoltage)
+            : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            _grade = grade;
+            _batteryVoltage = batteryVoltage;
+        }
+
+        // Method
+        public override string FormatForFile()
+        {
+            return $"{base.FormatForFile()};{_grade};{_batteryVoltage}";
+        }
+
+        public override string ToString()
+        {
+            return $"Item Number: {ItemNumber}\n" +
+                   $"Brand: {Brand}\n" +
+                   $"Quantity: {Quantity}\n" +
+                   $"Color: {Color}\n" +
+                   $"Price: {Price}\n" +
+                   $"Grade: {_grade}\n" +
+                   $"Battery Voltage: {_batteryVoltage}\n";
+        }
+    }
+}
